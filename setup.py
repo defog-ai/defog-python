b95dad2c7ff8cf2ea5dc525377ce0f38ba258d68
Rishabh Srivastava <rishabhsriv@gmail.com>
2023-01-13 21:03:26 -0800

    added additional description

--- setup.py
+++ setup.py
@@ -9,4 +9,8 @@ setup(
     install_requires=[
         'requests',
     ],
+    author_email="founders@defog.ai",
+    url="https://github.com/defog-ai/defog-python",
+    long_description='Defog is a Python library that helps you generate SQL queries from natural language questions. To use it, please sign up for an account at https://defog.ai. Then, you can use the API key to generate queries. You can view the source code for this library [https://github.com/defog-ai/defog-python](on GitHub).',
+    long_description_content_type='text/markdown'
 )
