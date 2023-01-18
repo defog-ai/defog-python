b33031ffcda7d66605d91e62729ac193a2c39a51
Rishabh Srivastava <rishabhsriv@gmail.com>
2023-01-17 22:55:39 -0800

    added more stuff to gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='defog',
     packages=find_packages(),
-    version='0.3.0',
+    version='0.4.0',
     description='Defog is a Python library that helps you generate SQL queries from natural language questions.',
     author='Full Stack Data Pte. Ltd.',
     license='MIT',
