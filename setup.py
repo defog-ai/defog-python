5c6c5fcf1d058287f97cdb665c4caf0efd4daec4
Rishabh Srivastava <rishabhsriv@gmail.com>
2023-01-18 23:34:55 -0800

    update query

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 setup(
     name='defog',
     packages=find_packages(),
-    version='0.5.0',
-    description='Defog is a Python library that helps you generate SQL queries from natural language questions.',
+    version='0.5.1',
+    description='Defog is a Python library that helps you generate data queries from natural language questions.',
     author='Full Stack Data Pte. Ltd.',
     license='MIT',
     install_requires=[
@@ -11,6 +11,6 @@ setup(
     ],
     author_email="founders@defog.ai",
     url="https://github.com/defog-ai/defog-python",
-    long_description='Defog is a Python library that helps you generate SQL queries from natural language questions. To use it, please sign up for an account at https://defog.ai/account and get an API key. Then, you can use the API key to generate queries.',
+    long_description='Defog is a Python library that helps you generate data queries from natural language questions. To use it, please sign up for an account at https://defog.ai/account and get an API key. Then, you can use the API key to generate queries.',
     long_description_content_type='text/markdown'
 )
