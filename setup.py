205e47c7f22ddbf86eb74a14455a8446686f8a9b
Rishabh Srivastava <rishabhsriv@gmail.com>
2023-01-29 17:33:22 -0800

    added versions to install requires

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import find_packages, setup
 setup(
     name='defog',
     packages=find_packages(),
-    version='0.9.1',
+    version='0.9.2',
     description='Defog is a Python library that helps you generate data queries from natural language questions.',
     author='Full Stack Data Pte. Ltd.',
     license='MIT',
     install_requires=[
-        'requests',
-        'psycopg2-binary',
-        'mysql-connector-python',
-        'pymongo',
-        'google-cloud-bigquery'
+        'requests>=2.28.2',
+        'psycopg2-binary>=2.9.5',
+        'mysql-connector-python>=8.0.32',
+        'pymongo>=4.3.3',
+        'google-cloud-bigquery>=3.4.2'
     ],
     author_email="founders@defog.ai",
     url="https://github.com/defog-ai/defog-python",
