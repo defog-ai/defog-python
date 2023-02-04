db4722bcdb76f73651da5669839a9659edb46a84
Rishabh Srivastava <rishabhsriv@gmail.com>
2023-02-03 17:16:02 -0800

    bumped up to v0.11

--- setup.py
+++ setup.py
@@ -2,16 +2,13 @@ from setuptools import find_packages, setup
 setup(
     name='defog',
     packages=find_packages(),
-    version='0.10.0',
+    version='0.11.0',
     description='Defog is a Python library that helps you generate data queries from natural language questions.',
     author='Full Stack Data Pte. Ltd.',
     license='MIT',
     install_requires=[
         'requests>=2.28.2',
         'psycopg2-binary>=2.9.5',
-        'mysql-connector-python>=8.0.32',
-        'pymongo>=4.3.3',
-        'google-cloud-bigquery>=3.4.2'
     ],
     author_email="founders@defog.ai",
     url="https://github.com/defog-ai/defog-python",
