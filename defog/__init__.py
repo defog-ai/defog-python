cde6dbefa0fcab996c89211db0d9bac78f29772e
Rishabh Srivastava <rishabhsriv@gmail.com>
2023-11-14 21:43:02 +0800

    formatting

--- defog/__init__.py
+++ defog/__init__.py
@@ -778,79 +778,6 @@ class Defog:
                 "Updating the schema for this database type via the library is not yet supported. If you are a premium user, please contact us and we will manually add it."
             )
 
-    def update_glossary(self, glossary: str = "", customized_glossary: dict = None):
-        """
-        Updates the glossary on the defog servers.
-        :param glossary: The glossary to be used.
-        """
-        r = requests.post(
-            "https://api.defog.ai/update_glossary",
-            json={
-                "api_key": self.api_key,
-                "glossary": glossary,
-                "customized_glossary": customized_glossary,
-            },
-        )
-        resp = r.json()
-        return resp
-
-    def get_glossary(self, mode="general"):
-        """
-        Gets the glossary on the defog servers.
-        """
-        r = requests.post(
-            "https://api.defog.ai/get_metadata",
-            json={"api_key": self.api_key},
-        )
-        resp = r.json()
-        if mode == "general":
-            return resp["glossary"]
-        elif mode == "customized":
-            return resp["customized_glossary"]
-
-    def get_metadata(self, format="markdown", export_path=None):
-        """
-        Gets the metadata on the defog servers.
-        """
-        r = requests.post(
-            "https://api.defog.ai/get_metadata",
-            json={"api_key": self.api_key},
-        )
-        resp = r.json()
-        items = []
-        for table in resp["table_metadata"]:
-            for item in resp["table_metadata"][table]:
-                item["table_name"] = table
-                items.append(item)
-        if format == "markdown":
-            return pd.DataFrame(items)[
-                ["table_name", "column_name", "data_type", "column_description"]
-            ].to_markdown(index=False)
-        elif format == "csv":
-            if export_path is None:
-                export_path = "metadata.csv"
-            pd.DataFrame(items)[
-                ["table_name", "column_name", "data_type", "column_description"]
-            ].to_csv(export_path, index=False)
-            print(f"Metadata exported to {export_path}")
-            return True
-
-    def get_feedback(self, n_rows: int = 50, start_from: int = 0):
-        """
-        Gets the feedback on the defog servers.
-        """
-        r = requests.post(
-            "https://api.defog.ai/get_feedback",
-            json={"api_key": self.api_key},
-        )
-        resp = r.json()
-        df = pd.DataFrame(resp["data"], columns=resp["columns"])
-        df["created_at"] = df["created_at"].apply(lambda x: x[:10])
-        for col in ["query_generated", "feedback_text"]:
-            df[col] = df[col].fillna("")
-            df[col] = df[col].apply(lambda x: x.replace("\n", "\\n"))
-        return df.iloc[start_from:].head(n_rows).to_markdown(index=False)
-
     def get_query(
         self,
         question: str,
@@ -1050,3 +977,76 @@ class Defog:
         )
         resp = r.json()
         return resp
+    
+    def update_glossary(self, glossary: str = "", customized_glossary: dict = None):
+        """
+        Updates the glossary on the defog servers.
+        :param glossary: The glossary to be used.
+        """
+        r = requests.post(
+            "https://api.defog.ai/update_glossary",
+            json={
+                "api_key": self.api_key,
+                "glossary": glossary,
+                "customized_glossary": customized_glossary,
+            },
+        )
+        resp = r.json()
+        return resp
+
+    def get_glossary(self, mode="general"):
+        """
+        Gets the glossary on the defog servers.
+        """
+        r = requests.post(
+            "https://api.defog.ai/get_metadata",
+            json={"api_key": self.api_key},
+        )
+        resp = r.json()
+        if mode == "general":
+            return resp["glossary"]
+        elif mode == "customized":
+            return resp["customized_glossary"]
+
+    def get_metadata(self, format="markdown", export_path=None):
+        """
+        Gets the metadata on the defog servers.
+        """
+        r = requests.post(
+            "https://api.defog.ai/get_metadata",
+            json={"api_key": self.api_key},
+        )
+        resp = r.json()
+        items = []
+        for table in resp["table_metadata"]:
+            for item in resp["table_metadata"][table]:
+                item["table_name"] = table
+                items.append(item)
+        if format == "markdown":
+            return pd.DataFrame(items)[
+                ["table_name", "column_name", "data_type", "column_description"]
+            ].to_markdown(index=False)
+        elif format == "csv":
+            if export_path is None:
+                export_path = "metadata.csv"
+            pd.DataFrame(items)[
+                ["table_name", "column_name", "data_type", "column_description"]
+            ].to_csv(export_path, index=False)
+            print(f"Metadata exported to {export_path}")
+            return True
+
+    def get_feedback(self, n_rows: int = 50, start_from: int = 0):
+        """
+        Gets the feedback on the defog servers.
+        """
+        r = requests.post(
+            "https://api.defog.ai/get_feedback",
+            json={"api_key": self.api_key},
+        )
+        resp = r.json()
+        df = pd.DataFrame(resp["data"], columns=resp["columns"])
+        df["created_at"] = df["created_at"].apply(lambda x: x[:10])
+        for col in ["query_generated", "feedback_text"]:
+            df[col] = df[col].fillna("")
+            df[col] = df[col].apply(lambda x: x.replace("\n", "\\n"))
+        return df.iloc[start_from:].head(n_rows).to_markdown(index=False)
