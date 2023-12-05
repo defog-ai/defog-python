c8cb6fe18837cefca6601f296eb22e52a509fec0
Rishabh Srivastava <rishabhsriv@gmail.com>
2023-12-05 16:37:40 +0800

    Rishabh/dx fixes (#15)
    * persist generate query url in the connection params
    
    * by default, add the auto-generate google sheet to the defog servers when defog gen is called
    
    * formatting
    
    * changed default mode of uploading metadata to local CSVs
    
    * improve error message
    
    * formatting
    
    * updated the `gen` function in the CLI to generate a CSV by default
    
    * fixed update function to use CSV

--- defog/cli.py
+++ defog/cli.py
@@ -190,19 +190,21 @@ def init():
         sys.exit(0)
     else:
         df = defog.Defog(api_key=api_key, db_type=db_type, db_creds=db_creds)
-        gsheets_url = df.generate_db_schema(table_name_list)
-        print("Your schema has been generated and is available at:\n")
-        print(f"\033[1m{gsheets_url}\033[0m\n")
+        filename = df.generate_db_schema(table_name_list)
+        print(
+            "Your schema has been generated and is available as a CSV file in this folder at:\n"
+        )
+        print(f"\033[1m{filename}\033[0m\n")
 
     print(
-        "You can give us more context about your schema at the above link. Once you're done, you can just hit enter to upload the data in this URL to Defog. If you would like to exit instead, just enter `exit`."
+        "You can give us more context about your schema by editing the CSV above. Once you're done, you can just hit enter to upload the data in the spreadsheet to Defog. If you would like to exit instead, just enter `exit`."
     )
     upload_option = prompt()
     if upload_option == "exit":
         print("Exiting.")
         sys.exit(0)
     else:
-        resp = df.update_db_schema(gsheets_url)
+        resp = df.update_db_schema_csv(filename)
         if resp["status"] == "success":
             print("Your schema has been updated. You're ready to start querying!")
         else:
@@ -255,11 +257,17 @@ def gen():
         table_name_list = re.split(r"\s+", table_names.strip())
     else:
         table_name_list = sys.argv[2:]
-    gsheets_url = df.generate_db_schema(table_name_list)
-    print("Your schema has been generated and is available at:\n")
-    print(f"\033[1m{gsheets_url}\033[0m\n")
+    filename = df.generate_db_schema(table_name_list)
     print(
-        "If you do modify the schema in the link provided, please run `defog update <url>` to update the updated schema."
+        "Your schema has been generated and is available at the following CSV file in this folder:\n"
+    )
+    print(f"\033[1m{filename}\033[0m\n")
+
+    print("We are now uploading this auto-generated schema to Defog.")
+    df.update_db_schema_csv(filename)
+
+    print(
+        "If you modify the auto-generated schema, please run `defog update <csv_filename>` again to refresh the schema on Defog's servers."
     )
 
 
@@ -270,15 +278,15 @@ def update():
     # check for 3rd arg (url), if not there, prompt user for url
     if len(sys.argv) < 3:
         print(
-            "defog update requires a google sheets url. Please enter the url of the google sheets document you would like to update:"
+            "defog update requires a CSV that contains your Database metadata. Please enter the path to the CSV you would like to update:"
         )
-        gsheets_url = prompt()
+        filename = prompt()
     else:
-        gsheets_url = sys.argv[2]
+        filename = sys.argv[2]
     # load config from .defog/connection.json
     df = defog.Defog()
     # upload schema to defog
-    resp = df.update_db_schema(gsheets_url)
+    resp = df.update_db_schema_csv(filename)
     if resp["status"] == "success":
         print("Your schema has been updated. You're ready to start querying!")
     else:
@@ -363,16 +371,23 @@ def query():
             user_question = query
             resp = df.run_query(query, retries=3)
             if not resp["ran_successfully"]:
-                print("Defog generated the following query to answer your question:\n")
-                print(f"\033[1m{resp['query_generated']}\033[0m\n")
+                if "query_generated" in resp:
+                    print(
+                        "Defog generated the following query to answer your question:\n"
+                    )
+                    print(f"\033[1m{resp['query_generated']}\033[0m\n")
 
-                print(
-                    f"However, your query did not run successfully. The error message generated while running the query on your database was\n\n\033[1m{resp['error_message']}\033[0m\n."
-                )
+                    print(
+                        f"However, your query did not run successfully. The error message generated while running the query on your database was\n\n\033[1m{resp['error_message']}\033[0m\n."
+                    )
 
-                print(
-                    f"If you continue to get these errors, please consider updating the metadata in your schema by editing the google sheet generated and running `defog update <url>`, or by updating your glossary.\n"
-                )
+                    print(
+                        f"If you continue to get these errors, please consider updating the metadata in your schema by editing the google sheet generated and running `defog update <url>`, or by updating your glossary.\n"
+                    )
+                else:
+                    print(
+                        f"Defog was unable to generate a query for your question. The error message generated while running the query on your database was\n\n\033[1m{resp.get('error_message')}\033[0m\n."
+                    )
                 query = prompt("Enter another query, or type 'e' to exit: ")
             else:
                 sql_generated = resp.get("query_generated")
