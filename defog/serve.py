4e289b2b7d89c1b5c41148605c9f06ba8ba54f90
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-01-23 00:41:33 +0800

    formatting

--- defog/serve.py
+++ defog/serve.py
@@ -7,6 +7,7 @@ import pandas as pd
 import os
 import json
 from io import StringIO
+
 # import requests
 # from tqdm import tqdm
 # import sys
@@ -59,9 +60,11 @@ app.add_middleware(
 home_dir = os.path.expanduser("~")
 defog_path = os.path.join(home_dir, ".defog")
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello, I am Defog"}
+
 
 @app.post("/generate_query")
 async def generate(request: Request):
@@ -73,40 +76,49 @@ async def generate(request: Request):
     resp = defog.run_query(question, previous_context=previous_context)
     return resp
 
+
 @app.post("/integration/get_tables_db_creds")
 async def get_tables_db_creds(request: Request):
     try:
         defog = Defog()
     except:
         return {"error": "no defog instance found"}
-    
+
     try:
         with open(os.path.join(defog_path, "tables.json"), "r") as f:
             table_names = json.load(f)
     except:
         table_names = []
-    
+
     try:
         with open(os.path.join(defog_path, "selected_tables.json"), "r") as f:
             selected_table_names = json.load(f)
     except:
         selected_table_names = []
-    
+
     db_type = defog.db_type
     db_creds = defog.db_creds
     api_key = defog.api_key
-    
-    return {"tables": table_names, "db_creds": db_creds, "db_type": db_type, "selected_tables": selected_table_names, "api_key": api_key}
+
+    return {
+        "tables": table_names,
+        "db_creds": db_creds,
+        "db_type": db_type,
+        "selected_tables": selected_table_names,
+        "api_key": api_key,
+    }
+
 
 @app.post("/integration/get_metadata")
 async def get_metadata(request: Request):
     try:
         with open(os.path.join(defog_path, "metadata.json"), "r") as f:
             metadata = json.load(f)
-        
+
         return {"metadata": metadata}
     except:
         return {"error": "no metadata found"}
+
 
 @app.post("/integration/generate_tables")
 async def get_tables(request: Request):
@@ -120,11 +132,12 @@ async def get_tables(request: Request):
 
     defog = Defog(api_key, db_type, db_creds)
     table_names = defog.generate_db_schema(tables=[], return_tables_only=True)
-    
+
     with open(os.path.join(defog_path, "tables.json"), "w") as f:
         json.dump(table_names, f)
-    
+
     return {"tables": table_names}
+
 
 @app.post("/integration/generate_metadata")
 async def generate_metadata(request: Request):
@@ -135,14 +148,19 @@ async def generate_metadata(request: Request):
         json.dump(tables, f)
 
     defog = Defog()
-    table_metadata = defog.generate_db_schema(tables=tables, scan=True, upload=True, return_format="csv_string")
-    metadata = pd.read_csv(StringIO(table_metadata)).fillna("").to_dict(orient="records")
-    
+    table_metadata = defog.generate_db_schema(
+        tables=tables, scan=True, upload=True, return_format="csv_string"
+    )
+    metadata = (
+        pd.read_csv(StringIO(table_metadata)).fillna("").to_dict(orient="records")
+    )
+
     with open(os.path.join(defog_path, "metadata.json"), "w") as f:
         json.dump(metadata, f)
-    
+
     defog.update_db_schema(StringIO(table_metadata))
     return {"metadata": metadata}
+
 
 @app.post("/integration/update_metadata")
 async def update_metadata(request: Request):
@@ -153,12 +171,14 @@ async def update_metadata(request: Request):
     defog.update_db_schema(StringIO(metadata))
     return {"status": "success"}
 
+
 @app.post("/instruct/get_glossary_golden_queries")
 async def update_glossary(request: Request):
     defog = Defog()
     glossary = defog.get_glossary()
     golden_queries = defog.get_golden_queries(format="json")
     return {"glossary": glossary, "golden_queries": golden_queries}
+
 
 @app.post("/instruct/update_glossary")
 async def update_glossary(request: Request):
@@ -167,6 +187,7 @@ async def update_glossary(request: Request):
     defog = Defog()
     defog.update_glossary(glossary=glossary)
     return {"status": "success"}
+
 
 @app.post("/instruct/update_golden_queries")
 async def update_golden_queries(request: Request):
