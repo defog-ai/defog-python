e5d124eeacb3ca110743c657e16731242c3704b5
jp <wongjingping@gmail.com>
2024-01-29 13:59:42 +0800

    Refactored to use params for values during sql execution to avoid injections Added character limit for possible values

--- defog/util.py
+++ defog/util.py
@@ -53,21 +53,50 @@ def write_logs(msg: str) -> None:
         pass
 
 
+def is_str_type(data_type: str) -> bool:
+    """
+    Check if the given data_type is a string type.
+
+    Args:
+        data_type (str): The data_type to check.
+
+    Returns:
+        bool: True if the data_type is a string type, False otherwise.
+    """
+    return data_type.lower().strip() in {
+        "character varying",
+        "text",
+        "character",
+        "varchar",
+        "char",
+        "string",
+    }
+
+
 def identify_categorical_columns(
     cur,  # a cursor object for any database
     table_name: str,
     rows: list,
+    distinct_threshold: int = 10,
+    character_length_threshold: int = 50,
 ):
     """
-    Identify categorical columns in the table and return the top 10 distinct values for each column.
+    Identify categorical columns in the table and return the top distinct values for each column.
 
     Args:
-        cur (cursor): A cursor object for any database.
+        cur (cursor): A cursor object for any database. This cursor should support the following methods:
+            - execute(sql, params)
+            - fetchone()
+            - fetchall()
         table_name (str): The name of the table.
-        rows (list): A list of dictionaries containing the column names and data types.
+        rows (list): A list of dictionaries containing the column names and data types.a
+        distinct_threshold (int): The threshold for the number of distinct values in a column to be considered categorical.
+        character_length_threshold (int): The threshold for the maximum length of a string column to be considered categorical.
+          This is a heuristic for pruning columns that might contain arbitrarily long strings like json / configs.
 
     Returns:
-        rows (list): The updated list of dictionaries containing the column names, data types and top 10 distinct values.
+        rows (list): The updated list of dictionaries containing the column names, data types and top distinct values.
+          The list is modified in-place.
     """
     # loop through each column, look at whether it is a string column, and then determine if it might be a categorical variable
     # if it is a categorical variable, then we want to get the distinct values and their counts
@@ -77,33 +106,28 @@ def identify_categorical_columns(
         f"Identifying categorical columns in {table_name}. This might take a while if you have many rows in your table."
     )
     for idx, row in enumerate(rows):
-        if row["data_type"].lower().strip() in [
-            "character varying",
-            "text",
-            "character",
-            "varchar",
-            "char",
-            "string",
-        ]:
+        if is_str_type(row["data_type"]):
             # get the total number of rows and number of distinct values in the table for this column
+            column_name = row["column_name"]
             cur.execute(
-                f"SELECT COUNT(DISTINCT {row['column_name']}) AS unique_count FROM {table_name};"
+                f"""SELECT COUNT(DISTINCT {column_name}) AS unique_count FROM {table_name} WHERE LENGTH({column_name}) < %s;""",
+                (character_length_threshold,),
             )
             try:
                 num_distinct_values = cur.fetchone()[0]
-            except:
+            except Exception as e:
                 num_distinct_values = 0
-
-            if num_distinct_values <= 10 and num_distinct_values > 0:
-                # get the top 10 distinct values
+            if num_distinct_values <= distinct_threshold and num_distinct_values > 0:
+                # get the top distinct_threshold distinct values
                 cur.execute(
-                    f"SELECT {row['column_name']}, COUNT({row['column_name']}) AS col_count FROM {table_name} GROUP BY {row['column_name']} ORDER BY col_count DESC LIMIT 10;"
+                    f"""SELECT {column_name}, COUNT({column_name}) AS col_count FROM {table_name} GROUP BY {column_name} ORDER BY col_count DESC LIMIT %s;""",
+                    (distinct_threshold,),
                 )
                 top_values = cur.fetchall()
                 top_values = [i[0] for i in top_values if i[0] is not None]
-                rows[idx]["top_values"] = ",".join(top_values)
+                rows[idx]["top_values"] = ",".join(sorted(top_values))
                 print(
-                    f"Identified {row['column_name']} as a likely categorical column. The unique values are: {top_values}"
+                    f"Identified {column_name} as a likely categorical column. The unique values are: {top_values}"
                 )
     return rows
 
