ced9dfe75df9968e9ef4a11b58bbf706fd09bf97
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-02-21 02:05:52 +0800

    fix minor cli bug

--- defog/cli.py
+++ defog/cli.py
@@ -379,10 +379,10 @@ def query():
             print("Defog generated the following query to answer your question:\n")
             print(f"\033[1m{resp['query_generated']}\033[0m\n")
             reason_for_query = resp.get("reason_for_query", "")
-            reason_for_query = reason_for_query.replace(". ", "\n\n")
-
-            print("This was its reasoning for generating this query:\n")
-            print(f"\033[1m{reason_for_query}\033[0m\n")
+            if reason_for_query:
+                reason_for_query = reason_for_query.replace(". ", "\n\n")
+                print("This was its reasoning for generating this query:\n")
+                print(f"\033[1m{reason_for_query}\033[0m\n")
 
             print("Results:\n")
             # print results in tabular format using 'columns' and 'data' keys
