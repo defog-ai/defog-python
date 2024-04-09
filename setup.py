c20b9e3ab54ef926687dd990d5cf6848a6c2510b
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-04-09 09:12:00 +0800

    Bump version number to 0.63.8 in setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     name="defog",
     packages=find_packages(),
     package_data={"defog": ["gcp/*", "aws/*"] + next_static_files},
-    version="0.63.7",
+    version="0.63.8",
     description="Defog is a Python library that helps you generate data queries from natural language questions.",
     author="Full Stack Data Pte. Ltd.",
     license="MIT",
