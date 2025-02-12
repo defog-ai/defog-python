7eda73f7184491b5b068e0fd6e17b7aabc6b7673
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-02-13 02:34:19 +0800

    remove un-needed test

--- tests/test_util.py
+++ tests/test_util.py
@@ -3,45 +3,6 @@ from unittest.mock import Mock, MagicMock, patch
 from defog.util import identify_categorical_columns, parse_update, get_feedback
 
 
-class TestParseUpdate(unittest.TestCase):
-    def test_parse_update_1_key_edit(self):
-        update_str = ["--app_name", "AWS"]
-        attributes_list = ["app_name"]
-        config = {"app_name": "GCP"}
-        expected_output = {"app_name": "AWS"}
-        self.assertEqual(
-            parse_update(update_str, attributes_list, config), expected_output
-        )
-
-    def test_parse_update_1_key_insert(self):
-        update_str = ["--app_name", "AWS"]
-        attributes_list = ["app_name"]
-        config = {"version": "1.0"}
-        expected_output = {"app_name": "AWS", "version": "1.0"}
-        self.assertEqual(
-            parse_update(update_str, attributes_list, config), expected_output
-        )
-
-    def test_parse_update_1_key_not_exists(self):
-        update_str = ["--app_name", "AWS"]
-        attributes_list = ["version"]
-        config = {"app_name": "GCP"}
-        expected_output = config
-        self.assertEqual(
-            parse_update(update_str, attributes_list, config), expected_output
-        )
-        config = {"version": "2.0"}
-        expected_output = config
-        self.assertEqual(
-            parse_update(update_str, attributes_list, config), expected_output
-        )
-        config = {"random_key": "random_value"}
-        expected_output = config
-        self.assertEqual(
-            parse_update(update_str, attributes_list, config), expected_output
-        )
-
-
 class TestIdentifyCategoricalColumns(unittest.TestCase):
     def setUp(self):
         self.cur = Mock()
