f3626f9d883d4c81af1a55223c75a1723e1838d0
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-06-20 17:59:44 +0800

    remove feedback tests as we do not have that anymore

--- tests/test_util.py
+++ tests/test_util.py
@@ -66,55 +66,5 @@ if __name__ == "__main__":
     unittest.main()
 
 
-class TestGetFeedback(unittest.TestCase):
-    @patch("defog.util.prompt", return_value="y")
-    @patch("requests.post")
-    def test_positive_feedback(self, mock_post, mock_prompt):
-        get_feedback("api_key", "db_type", "user_question", "sql_generated", "base_url")
-        assert mock_post.call_count == 1
-        self.assertIn("good", mock_post.call_args.kwargs["json"]["feedback"])
-        self.assertNotIn("feedback_text", mock_post.call_args.kwargs["json"])
-
-    @patch("defog.util.prompt", side_effect=["n", "bad query", "", "", ""])
-    @patch("requests.post")
-    def test_negative_feedback_with_text(self, mock_post, mock_prompt):
-        get_feedback("api_key", "db_type", "user_question", "sql_generated", "base_url")
-        # 2 calls: 1 to /feedback, 1 to /reflect_on_error
-        assert mock_post.call_count == 2
-        self.assertIn("api_key", mock_post.call_args.kwargs["json"]["api_key"])
-        self.assertIn("user_question", mock_post.call_args.kwargs["json"]["question"])
-        self.assertIn(
-            "sql_generated", mock_post.call_args.kwargs["json"]["sql_generated"]
-        )
-        self.assertIn("bad query", mock_post.call_args.kwargs["json"]["error"])
-
-    @patch("defog.util.prompt", side_effect=["n", "", "", "", ""])
-    @patch("requests.post")
-    def test_negative_feedback_without_text(self, mock_post, mock_prompt):
-        get_feedback("api_key", "db_type", "user_question", "sql_generated", "base_url")
-        # 2 calls: 1 to /feedback, 1 to /reflect_on_error
-        assert mock_post.call_count == 2
-        self.assertIn("api_key", mock_post.call_args.kwargs["json"]["api_key"])
-        self.assertIn("user_question", mock_post.call_args.kwargs["json"]["question"])
-        self.assertIn(
-            "sql_generated", mock_post.call_args.kwargs["json"]["sql_generated"]
-        )
-        self.assertIn("", mock_post.call_args.kwargs["json"]["error"])
-
-    @patch("defog.util.prompt", side_effect=["invalid", "y"])
-    @patch("requests.post")
-    def test_invalid_then_valid_input(self, mock_post, mock_prompt):
-        get_feedback("api_key", "db_type", "user_question", "sql_generated", "base_url")
-        assert mock_post.call_count == 1
-        self.assertIn("good", mock_post.call_args.kwargs["json"]["feedback"])
-        self.assertNotIn("feedback_text", mock_post.call_args.kwargs["json"])
-
-    @patch("defog.util.prompt", side_effect=[""])
-    @patch("requests.post")
-    def test_skip_input(self, mock_post, mock_prompt):
-        get_feedback("api_key", "db_type", "user_question", "sql_generated", "base_url")
-        mock_post.assert_not_called()
-
-
 if __name__ == "__main__":
     unittest.main()
