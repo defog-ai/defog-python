57a54a45e5433c9aa8da8549771f6ca14c4b2caa
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-06-26 09:24:53 +0800

    version bump

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ extras = {
 setup(
     name="defog",
     packages=find_packages(),
-    version="1.1.6",
+    version="1.1.7",
     description="Defog is a Python library that helps you generate data queries from natural language questions.",
     author="Full Stack Data Pte. Ltd.",
     license="MIT",
